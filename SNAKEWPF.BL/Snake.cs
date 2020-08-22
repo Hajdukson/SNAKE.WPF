081e513e9696b3da860158a2083a7a245d3d9204
Michal Hajduk <michal.hajudk@gmail.com>
2020-08-22 14:27:23 +0200

    can move the  snake

--- SNAKEWPF.BL/Snake.cs
+++ SNAKEWPF.BL/Snake.cs
@@ -5,59 +5,37 @@ using System.Windows.Shapes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace SNAKEWPF.BL
 {
     public class Snake
     {
-        public int Length { get; private set; } = 5;
-        public Direction Direction { get; set; } = Direction.Right;
-        public Coordinate HeadPosition { get; private set; } = new Coordinate(1, 1);
-        public List<Coordinate> Tail { get; private set; } = new List<Coordinate>();
-        public List<Rectangle> Waz { get; set; } = new List<Rectangle>();
-        public Snake()
+        public List<Coordinate> Tail { get; set; } = new List<Coordinate>
         {
+            new Coordinate(1,1)
+        };
+        public Direction Direction { get; set; } = Direction.Right;
 
-        }
-
-        public void EatFruit()
-        {   
-            Length += 1;
-            Rectangle newRectangle = new Rectangle
-            {
-                Height = 10,
-                Width = 10,
-                Fill = Brushes.Red
-            };
-            Waz.Add(newRectangle);
-        }
         public void Move()
         {
             switch (Direction)
             {
                 case Direction.Left:
-                    HeadPosition.X--;
+                    Tail[0].X -= 10;
                     break;
                 case Direction.Right:
-                    HeadPosition.X++;
+                    Tail[0].X += 10;
                     break;
                 case Direction.Up:
-                    HeadPosition.Y--;
+                    Tail[0].Y -= 10;
                     break;
                 case Direction.Down:
-                    HeadPosition.Y++;
+                    Tail[0].Y += 10;
                     break;
-            }
-        }
-        public void TailLogic()
-        {
-            Tail.Add(new Coordinate(HeadPosition.X, HeadPosition.Y));
-            if (Tail.Count > Length)
-            {
-                var endTail = Tail.First();
-                Tail.Remove(endTail);
             }
         }
     }
     public enum Direction { Left, Right, Up, Down }
+
 }
--- SNAKEWPF/MainWindow.xaml.cs
+++ SNAKEWPF/MainWindow.xaml.cs
@@ -29,83 +29,26 @@ namespace SNAKEWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer _time;
-        Fruit _fruit;
         Snake _snake;
-        //bool Exit = false;
-        //private int points = 0;
-        private bool outOfRange = false;
-        private bool GameOver
-        {
-            get
-            {
-                return (_snake.Tail.Where(c => c.X == _snake.HeadPosition.X
-                && c.Y == _snake.HeadPosition.Y).ToList().Count > 1) || outOfRange;
-            }
-        }
+        Fruit _fruit;
+        DispatcherTimer _time;
         public MainWindow()
         {
             InitializeComponent();
-            _time = new DispatcherTimer();
-
-            _snake = new Snake();
             _fruit = new Fruit();
-            
-            mycanvas.Children.Insert(0, _fruit.Circle);
-            SetSnakeInCanvas();
-            
-            myImage.Source = RandomImageSource();
+            _snake = new Snake();
+            _time = new DispatcherTimer();
             _time.Interval = new TimeSpan(0, 0, 0, 0, 100);
             _time.Tick += Time_Tick;
-            
-        }
-        private void SetSnakeInCanvas()
-        {
-            Rectangle newRectangle = new Rectangle
-            {
-                Height = 10,
-                Width = 10,
-                Fill = Brushes.Red
-            };
-
-            _snake.Waz.Add(newRectangle);
-
-            Canvas.SetLeft(_snake.Waz.ElementAt(0), _snake.HeadPosition.X);
-            Canvas.SetTop(_snake.Waz.ElementAt(0), _snake.HeadPosition.Y);
-
-            mycanvas.Children.Insert(0, _snake.Waz.ElementAt(0));
-        }
-        private ImageSource RandomImageSource()
-        {
-            var urlList = new List<Uri>
-            {
-                new Uri("https://cdn.pixabay.com/photo/2015/02/28/15/25/snake-653639_1280.jpg"),
-                new Uri("https://cdn.pixabay.com/photo/2014/11/23/21/22/green-tree-python-543243_1280.jpg"),
-                new Uri("https://cdn.pixabay.com/photo/2014/10/25/07/52/king-snake-502263_1280.jpg"),
-                new Uri("https://cdn.pixabay.com/photo/2015/02/28/15/25/rattlesnake-653642_1280.jpg"),
-                new Uri("https://cdn.pixabay.com/photo/2015/02/28/15/25/rattlesnake-653646_1280.jpg")
-            };
-
-            var random = new Random();
-            var drawnImg = random.Next(0, urlList.Count - 1);
-            var bitmap = new BitmapImage(urlList.ElementAt(drawnImg));
-
-            return bitmap;
         }
         private void Time_Tick(object sender, EventArgs e)
         {
             _snake.Move();
-        }
 
-        //private bool FruitRespOnSnakeTail(Fruit fruit)
-        //{
-        //    foreach (Coordinate tailElement in _snake.Tail)
-        //    {
-        //        if (tailElement.X == fruit.FruitCoordinate.X && tailElement.Y == fruit.FruitCoordinate.Y)
-        //            return true;
-        //    }
-        //    return false;
-        //}
+            SetSnakeInCanvas();
+            myCanvas.Children.RemoveAt(1);
+
+        }
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.Key)
@@ -123,6 +66,46 @@ namespace SNAKEWPF
                     _snake.Direction = Direction.Right;
                     break;
             }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetSnakeInCanvas();
+            SetFruitInCanvas();
+            _time.Start();
+        }
+        private void SetFruitInCanvas()
+        {
+            myCanvas.Children.Insert(0, _fruit.Circle);
+        }
+        private void SetSnakeInCanvas()
+        {
+            Rectangle newRectangle = new Rectangle
+            {
+                Height = 10,
+                Width = 10,
+                Fill = Brushes.Red
+            };
+
+            Canvas.SetLeft(newRectangle, _snake.Tail[0].X);
+            Canvas.SetTop(newRectangle, _snake.Tail[0].Y);
+            myCanvas.Children.Add(newRectangle);
+        }
+        private ImageSource RandomImageSource()
+        {
+            var urlList = new List<Uri>
+            {
+                new Uri("https://cdn.pixabay.com/photo/2015/02/28/15/25/snake-653639_1280.jpg"),
+                new Uri("https://cdn.pixabay.com/photo/2014/11/23/21/22/green-tree-python-543243_1280.jpg"),
+                new Uri("https://cdn.pixabay.com/photo/2014/10/25/07/52/king-snake-502263_1280.jpg"),
+                new Uri("https://cdn.pixabay.com/photo/2015/02/28/15/25/rattlesnake-653642_1280.jpg"),
+                new Uri("https://cdn.pixabay.com/photo/2015/02/28/15/25/rattlesnake-653646_1280.jpg")
+            };
+
+            var random = new Random();
+            var drawnImg = random.Next(0, urlList.Count - 1);
+            var bitmap = new BitmapImage(urlList.ElementAt(drawnImg));
+
+            return bitmap;
         }
     }
 }
